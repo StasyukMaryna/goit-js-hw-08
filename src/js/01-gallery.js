fa1f634ca3cbe1bf7c35344cd36c29b1499b6db5
StasyukMaryna <stasyuk.m.v@gmail.com>
2023-06-02 22:35:11 +0200

    1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,38 @@
-// Add imports above this line
 import { galleryItems } from './gallery-items';
-// Change code below this line
 
 console.log(galleryItems);
+import simpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+const listGallery = document.querySelector('.gallery');
+
+function createGalleryEl(items) {
+  let galleryElements = items.map(item => {
+    let itemEl = document.createElement('li');
+    itemEl.classList.add('gallery__item');
+
+    let linkEl = document.createElement('a');
+    linkEl.classList.add('gallery__link');
+    linkEl.href = item.original;
+    itemEl.appendChild(linkEl);
+
+    let imgEl = document.createElement('img');
+    imgEl.classList.add('gallery__image');
+    imgEl.src = item.preview;
+    imgEl.alt = item.description;
+
+    linkEl.appendChild(imgEl);
+
+    return itemEl;
+  });
+  listGallery.append(...galleryElements);
+}
+
+function initGallery(items) {
+  let galleryElements = createGalleryEl(items);
+  const captions = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: '250',
+    // captionType: "alt",
+  });
+}
+initGallery(galleryItems);
